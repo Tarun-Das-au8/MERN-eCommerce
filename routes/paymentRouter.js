7c4725d9363cdeacda08253c997ea78b6b476b37
Tarun Das <imtarun79@gmail.com>
2021-02-08 20:25:39 +0530

    payment routes

---
+++ routes/paymentRouter.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const paymentControl = require('../controllers/paymentControl')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
+
+
+router.route('/payment')
+    .get(auth, authAdmin, paymentControl.getPayments)
+    .post(auth, paymentControl.createPayment)
+
+
+module.exports = router
